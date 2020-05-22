9a259f1d11d3e8ff99927a48aa809d0eb8ca780e
davidparker1 <davidparker@flywheel.io>
2020-05-22 13:56:39 -0400

    bash worls with realpath or readlink

--- commands/bash_crawl.sh
+++ commands/bash_crawl.sh
@@ -5,12 +5,22 @@ IFS=':'
 read -ra paths <<< "$PATH"
 for i in "${paths[@]}"; do
   #ls -1 $i/pip* 2> /dev/null | grep -E pip[0-9]?.?[0-9]?$
-  a=`command -v realpath`
+  
+  a=$(command -v realpath)
+  b=$(command -v readlink)
+  
   if [ ! -z $a ]; then
     
     for p in ${i}/${1}; do
       #echo "1:$p"
       echo `realpath $p`
+    done
+    
+  elif [ ! -z $b ]; then
+    
+    for p in ${i}/${1}; do
+      #echo "1:$p"
+      echo `readlink -f $p`
     done
     
   else
