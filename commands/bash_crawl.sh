34a28606a23ffc3367747973d56037c8aa01acdb
davidparker1 <davidparker@flywheel.io>
2020-05-22 10:41:55 -0400

    Bash deals with symlinks

--- commands/bash_crawl.sh
+++ commands/bash_crawl.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 shopt -s extglob
-echo $PATH
+#echo $PATH
 IFS=':'
 read -ra paths <<< "$PATH"
 for i in "${paths[@]}"; do
   #ls -1 $i/pip* 2> /dev/null | grep -E pip[0-9]?.?[0-9]?$
-  ls -1 ${i}/${1} 2> /dev/null
+  a=`command -v realpath`
+  if [ ! -z $a ]; then
+    
+    for p in ${i}/${1}; do
+      #echo "1:$p"
+      echo `realpath $p`
+    done
+    
+  else
+    ls -1 ${i}/${1} 2> /dev/null
+  fi
+  
 done
