b74c2bf322e83ecd3fa7123df08aa50945090457
dpark6060 <dpark6060@gmail.com>
2021-10-07 12:11:28 -0400

    added example site_list.py

---
+++ example_site_list.py
@@ -0,0 +1,14 @@
+def get_site_list():
+    site_list = {'Stanford CNI':
+                     ['cni.flywheel.io',
+                      'davidparker@flywheel.io',
+                      '<API KEY>'],
+                 
+                 'ss.ce':
+                     ['ss.ce.flywheel.io',
+                      'davidparker@flywheel.io',
+                      '<API KEY>'],
+                 .. etc
+                 }
+
+    return(site_list)
